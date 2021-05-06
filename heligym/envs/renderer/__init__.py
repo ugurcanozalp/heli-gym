20682f87109d922e808797d2b54ed0d721c56688
MGokcayK <gkcy.kbts@gmail.com>
2021-05-06 12:54:26 +0100

    linux compatible so files

--- heligym/envs/renderer/__init__.py
+++ heligym/envs/renderer/__init__.py
@@ -4,9 +4,9 @@ import sys
 if sys.platform == 'win32':
     os.environ['PATH'] += os.pathsep + os.path.dirname(os.path.realpath(__file__)) + "/python"
     os.environ['PATH'] += os.pathsep + os.path.dirname(os.path.realpath(__file__)) + "/resources/models"
-elif sys.platform == 'linux':
-    packagedir = os.path.dirname(os.path.abspath(__file__))
-    os.environ['HELIGYM_PYTHON_DIR'] = os.path.join(packagedir, 'python')
-    os.environ['HELIGYM_MODEL_DIR'] = os.path.join(packagedir, 'resources/models')    
+
+packagedir = os.path.dirname(os.path.abspath(__file__))
+os.environ['HELIGYM_PYTHON_DIR'] = os.path.join(packagedir, 'python')
+os.environ['HELIGYM_MODEL_DIR'] = os.path.join(packagedir, 'resources/models')    
 
 
