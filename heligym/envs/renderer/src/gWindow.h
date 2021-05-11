b739bc0950240c002b65527b49daa576abcbf4e2
MGokcayK <gkcy.kbts@gmail.com>
2021-05-11 23:57:00 +0300

    ubuntu 16.04.7

--- heligym/envs/renderer/src/gWindow.h
+++ heligym/envs/renderer/src/gWindow.h
@@ -4,11 +4,11 @@
 #include "camera.h"
 #include "model.h"
 
+#include <GLFW/glfw3.h>
+
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
-
-#include <GLFW/glfw3.h>
 
 #include <iostream>
 
