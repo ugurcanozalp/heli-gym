f1e5af8d239617cd32df71accf0f38f6a27130e0
MGokcayK <gkcy.kbts@gmail.com>
2021-05-11 19:14:52 +0100

    Fixing `linux` LD_LIBRARY_PATH for calling related libs

--- heligym/envs/renderer/__init__.py
+++ heligym/envs/renderer/__init__.py
@@ -1,13 +1,25 @@
+#!/usr/bin/python3
 import os 
 import sys
 
-if sys.platform == 'win32':
-    os.environ['PATH'] += os.pathsep + os.path.dirname(os.path.realpath(__file__)) + "/libs"
-    os.environ['PATH'] += os.pathsep + os.path.dirname(os.path.realpath(__file__)) + "/python"
-    os.environ['PATH'] += os.pathsep + os.path.dirname(os.path.realpath(__file__)) + "/resources/models"
-
 packagedir = os.path.dirname(os.path.abspath(__file__))
+os.environ['HELIGYM_LIBS_DIR'] = os.path.join(packagedir, 'libs')
 os.environ['HELIGYM_PYTHON_DIR'] = os.path.join(packagedir, 'python')
 os.environ['HELIGYM_MODEL_DIR'] = os.path.join(packagedir, 'resources/models')    
 
+if sys.platform == 'win32':
+    os.environ['PATH'] += os.pathsep + os.environ['HELIGYM_LIBS_DIR']
+    os.environ['PATH'] += os.pathsep + os.environ['HELIGYM_PYTHON_DIR']
+    os.environ['PATH'] += os.pathsep + os.environ['HELIGYM_MODEL_DIR']
+
+elif sys.platform == 'linux':
+    if 'LD_LIBRARY_PATH' not in os.environ:
+        os.environ['LD_LIBRARY_PATH']  = os.pathsep + os.environ['HELIGYM_LIBS_DIR']
+        os.environ['LD_LIBRARY_PATH'] += os.pathsep + os.environ['HELIGYM_PYTHON_DIR']
+        os.environ['LD_LIBRARY_PATH'] += os.pathsep + os.environ['HELIGYM_MODEL_DIR']
+        try:
+            os.execv(sys.executable, [sys.executable] + sys.argv)
+        except Exception as e:
+            print(e)
+            sys.exit(1)
 
